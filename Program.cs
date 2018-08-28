20d7cc7c2ab388b681c59ee8c95536619e0c48ac
Joey <smithjoey5123@gmail.com>
2018-08-28 11:33:35 -0500

    family-dictionary practice

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+                 myFamily.Add("Sister", new Dictionary<string, string>(){ 
+                {"name", "Sasha"},
+                {"age", "30"}
+            });
+                 myFamily.Add("Mother", new Dictionary<string, string>(){ 
+                {"name", "Shawna"},
+                {"age", "47"}
+            });
+                 myFamily.Add("Father", new Dictionary<string, string>(){ 
+                {"name", "Allan"},
+                {"age", "52"}
+            });
+                 myFamily.Add("Dog", new Dictionary<string, string>(){ 
+                {"name", "Oliver"},
+                {"age", "4"}
+            });
+             foreach (var familyMember in myFamily)
+            {
+                Console.WriteLine($"My {familyMember.Key}'s name is {familyMember.Value["name"]} and is {familyMember.Value["age"]}");
+            }
+        }
+    }
+}
